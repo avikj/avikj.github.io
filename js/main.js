0f449a723ab9d2ebb40c69289e6533ce8e21f4ac
avikj <avikjain12345@gmail.com>
2016-02-15 09:33:27 -0800

    Added icons and basic content

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@
         typeSpeed: 0,
         loop: true
       });
+      
+      var colors = ["#3883FA", "#EE422E", "#FFBC00", "#2FA851"];
+      $('i').hover(function(){
+          var newColor = colors[(Math.random()*4)|0];
+          $(this).animate({color: newColor}, 100);
+      }, function(){
+          $(this).animate({color: "#000000"}, 100)
+      });
   });
