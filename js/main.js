c029188b60d608909be477daf42271605b639b81
avikj <avikjain12345@gmail.com>
2016-02-14 20:34:16 -0800

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
-			$(function(){
-				$.fn.extend({
-				    animateCss: function (animationName) {
-				        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-				        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
-				            $(this).removeClass('animated ' + animationName);
-				        });
-				    }
-				});
-				var b1 = $('#b1');
-				b1.animateCss('bounce');
-				console.log("deez"+b1);
-				b1.onclick = function(){console.log('clicked');}
-				console.log('added listener');
-			});
+  $(function(){
+      $("#typedHeader").typed({
+        strings: ["a developer", "a hacker"],
+        typeSpeed: 0,
+        loop: true
+      });
+  });
