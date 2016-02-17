4ca3608609012381290028d470980e5345018276
avikj <avikjain12345@gmail.com>
2016-02-16 23:12:38 -0800

    Added content and WOW.js

--- js/main.js
+++ js/main.js
@@ -13,4 +13,31 @@
           $(this).animate({color: "#000000"}, 100)
       });
       $('.ui.accordion').accordion();
+
+      var imgs = [$('#swiftImg'), $('#flappyImg'), $('#quickImg')];
+      var imgSrcs = ['pebbleapp.gif', 'flappynerd.gif', 'quickchat.gif'];
+
+      var swiftGif = new Image();
+
+          swiftGif.src = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/'))+'/img/'+imgSrcs[0];
+          swiftGif.onload = function(){
+          imgs[0].attr("src", this.src);   
+          console.log('loaded image '+0);
+      };
+
+      var flappyGif = new Image();
+
+      flappyGif.src = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/'))+'/img/'+imgSrcs[1];
+      flappyGif.onload = function(){
+          imgs[1].attr("src", this.src);   
+          console.log('loaded image '+1);
+      };
+
+      var quickGif = new Image();
+
+      quickGif.src = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/'))+'/img/'+imgSrcs[2];
+      quickGif.onload = function(){
+          imgs[2].attr("src", this.src);   
+          console.log('loaded image '+2);
+      };
   });
