050582b8c662705412090bd43378b975012d045e
avikj <avikjain12345@gmail.com>
2016-02-18 21:50:57 -0800

    Added site footer and adjusted animations to prevent scrollbar issues

--- js/main.js
+++ js/main.js
@@ -12,7 +12,29 @@
       }, function(){
           $(this).animate({color: "#000000"}, 100)
       });
+
+      $('.sqicon').hover(function(){
+          var newColor = colors[(Math.random()*4)|0];
+          $(this).animate({color: newColor}, 100);
+      }, function(){
+          $(this).animate({color: "#ffffff"}, 100)
+      });
       $('.ui.accordion').accordion();
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+
+
 
       var imgs = [$('#swiftImg'), $('#flappyImg'), $('#quickImg')];
       var imgSrcs = ['pebbleapp.gif', 'flappynerd.gif', 'quickchat.gif'];
@@ -40,4 +62,5 @@
           imgs[2].attr("src", this.src);   
           console.log('loaded image '+2);
       };
+
   });
